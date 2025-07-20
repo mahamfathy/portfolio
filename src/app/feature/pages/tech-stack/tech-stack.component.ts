ffdd9724c678896227651833fb41f8d14600c958
mahamfathy <mahafathyh@gmail.com>
2025-07-20 16:29:12 +0300

    [feat] tech stack component: apply animation

--- src/app/feature/pages/tech-stack/tech-stack.component.ts
+++ src/app/feature/pages/tech-stack/tech-stack.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
 interface Tech {
   name: string;
   iconClass: string;
 }
+
 @Component({
   selector: 'app-tech-stack',
   templateUrl: './tech-stack.component.html',
   styleUrls: ['./tech-stack.component.scss'],
+  animations: [
+    trigger('fadeInAnimation', [
+      transition('hidden => visible', [
+        query(
+          'span',
+          [
+            style({ opacity: 0, transform: 'translateY(30px)' }),
+            stagger(100, [
+              animate(
+                '600ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
-export class TechStackComponent {
+export class TechStackComponent implements OnInit {
+  animationState: 'hidden' | 'visible' = 'hidden';
+
+  @ViewChild('stackIcons') stackIcons!: ElementRef;
+
   techStack: Tech[] = [
     { name: 'HTML', iconClass: 'devicon-html5-plain' },
     { name: 'CSS', iconClass: 'devicon-css3-plain' },
@@ -26,4 +65,24 @@ export class TechStackComponent {
     { name: 'Jira', iconClass: 'devicon-jira-plain' },
     { name: 'Docker', iconClass: 'devicon-docker-plain' },
   ];
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    setTimeout(() => this.checkIfInView(), 0);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkIfInView();
+  }
+
+  checkIfInView() {
+    if (!this.stackIcons) return;
+    const rect = this.stackIcons.nativeElement.getBoundingClientRect();
+    const isVisible = rect.top <= window.innerHeight - 100;
+    if (isVisible && this.animationState !== 'visible') {
+      this.animationState = 'visible';
+    }
+  }
 }
